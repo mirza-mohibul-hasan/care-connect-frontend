32b8f62e0fbeeea9f89f4c1eed5a11cd85b59763
nishattandra <nishattandra2001@gmail.com>
2023-11-07 01:15:14 +0600

    Slider added

--- src/pages/Home/HomeComponents/Banner.jsx
+++ src/pages/Home/HomeComponents/Banner.jsx
@@ -1,6 +1,7 @@
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { BsArrowRight } from 'react-icons/bs';
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 const Banner = () => {
@@ -20,15 +21,67 @@ const Banner = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <div className="hero" >
+            <div className="hero-content flex justify-evenly">
+              <img src="https://i.ibb.co/yqR48qM/The-Connection-Between-Poverty-and-Health.jpg" className="max-w-lg h-[400px] ml-10 rounded-lg mb-10 " />
+              <div className='w-1/2 ml-10 '>
+                <h1 className="text-4xl font-bold text-black ">The Largest  <span className='text-blue-800'> Health Care</span>  <br /> Website</h1>
+                <p className="py-6 font-semibold text-gray-950 mr-10">Here we provide our best services. We help the helpless people and give the best solution about their health.</p>
+                <button className="btn bg-blue-800 text-white font-bold border-0">Explore More <span className='ml-3'><BsArrowRight></BsArrowRight></span></button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="hero" >
+            <div className="hero-content flex justify-evenly">
+              <img src="https://i.ibb.co/J2BrkNf/img.webp" className="max-w-lg h-[400px] ml-10 rounded-lg" />
+              <div className='w-1/2 ml-10 '>
+                <h1 className="text-4xl font-bold text-black ">The Largest  <span className='text-blue-800'> Health Care</span>  <br /> Website</h1>
+                <p className="py-6 font-semibold text-gray-950 mr-10">Here we provide our best services. We help the helpless people and give the best solution about their health.</p>
+                <button className="btn bg-blue-800 text-white font-bold border-0">About Us <span className='ml-3'><BsArrowRight></BsArrowRight></span></button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="hero" >
+            <div className="hero-content flex justify-evenly">
+              <img src="https://i.ibb.co/hWrz2rY/zeyn-afuang-9xp0-AWvl-GC4-unsplash.jpg" className="max-w-lg h-[400px] ml-10 rounded-lg" />
+              <div className='w-1/2 ml-10 '>
+                <h1 className="text-4xl font-bold text-black ">The Largest  <span className='text-blue-800'> Health Care</span>  <br /> Website</h1>
+                <p className="py-6 font-semibold text-gray-950 mr-10">Here we provide our best services. We help the helpless people and give the best solution about their health.</p>
+                <button className="btn bg-blue-800 text-white font-bold border-0">Explore More <span className='ml-3'><BsArrowRight></BsArrowRight></span></button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="hero" >
+            <div className="hero-content flex justify-evenly">
+              <img src="https://i.ibb.co/6yLfkjH/istockphoto-1129628379-612x612.jpg" className="max-w-lg h-[400px] ml-10 rounded-lg" />
+              <div className='w-1/2 ml-10 '>
+                <h1 className="text-4xl font-bold text-black ">The Largest  <span className='text-blue-800'> Health Care</span>  <br /> Website</h1>
+                <p className="py-6 font-semibold text-gray-950 mr-10">Here we provide our best services. We help the helpless people and give the best solution about their health.</p>
+                <button className="btn bg-blue-800 text-white font-bold border-0">Explore More <span className='ml-3'><BsArrowRight></BsArrowRight></span></button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="hero" >
+            <div className="hero-content flex justify-evenly">
+              <img src="https://i.ibb.co/XsQF9W8/Cancer-patient-with-mask-resized.jpg" className="max-w-lg h-[400px] ml-10 rounded-lg" />
+              <div className='w-1/2 ml-10 '>
+                <h1 className="text-4xl font-bold text-black ">The Largest  <span className='text-blue-800'> Health Care</span>  <br /> Website</h1>
+                <p className="py-6 font-semibold text-gray-950 mr-10">Here we provide our best services. We help the helpless people and give the best solution about their health.</p>
+                <button className="btn bg-blue-800 text-white font-bold border-0">Explore More <span className='ml-3'><BsArrowRight></BsArrowRight></span></button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+
       </Swiper>
     </div>
   );
